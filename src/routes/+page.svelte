4171964260ec33cbdd0f7d2637900234837aaf41
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2024-11-24 23:55:32 +0100

    🎉 (Initial commit): Create basic simulation capabilities

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import FireGrid from '../lib/FireGrid.svelte';
+</script>
+
+<FireGrid />
+
+<style>
+	:global(body) {
+		margin: 0;
+		font-family: cursive;
+	}
+
+	:global(input) {
+		font-family: inherit;
+		font-size: 0.9em;
+	}
+
+	:global(button) {
+		font-family: inherit;
+		font-size: 1.1em;
+	}
+</style>
