6e71b6677ce63ce2ad69945739627d10341572f6
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2025-05-16 16:03:39 +0200

    🚀 (Environment variables): Update the import to accept a dynamic value for DGRAPH_URL

--- src/lib/dgraph.ts
+++ src/lib/dgraph.ts
@@ -1,5 +1,7 @@
 import { DgraphClientStub, DgraphClient } from 'dgraph-js';
-import { DGRAPH_URL } from '$env/static/private'; 
+// Load DGRAPH_URL dynamically so that the value passed in docker-compose.yml
+// is taken into account when running the container
+import { env } from '$env/dynamic/private';
 
-const clientStub = new DgraphClientStub(DGRAPH_URL);
+const clientStub = new DgraphClientStub(env.DGRAPH_URL);
 export const db = new DgraphClient(clientStub);
